dbb99a3f244fde30a7a16a2ce41c5f36d4a4a871
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2024-02-21 13:02:00 +0100

    use new vsr53 library

--- test/test_serial.py
+++ test/test_serial.py
@@ -1,21 +1,8 @@
 from __future__ import annotations
 
-from pyvsr53dl.vsr53dl import PyVSR53DL
-
-import slow_control
+from slow_control.devices.serial import get_ports
 
 
 def test_ports():
-    ports = slow_control.devices.serial.get_ports()
+    ports = get_ports()
     print(ports)
-
-
-# TODO: stuck
-def test_vsr53usb():
-    port = "COM10"
-    address = 1
-    sensor = PyVSR53DL(port, address)
-    sensor.open_communication()
-    device_type = sensor.get_device_type()
-    print(device_type)
-    sensor.close_communication()
