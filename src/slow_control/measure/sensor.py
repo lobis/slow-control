10421c43218abdbea884d7bb52122c2fa7b48c02
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2024-02-22 10:26:53 +0100

    working with pressure sensor

--- src/slow_control/measure/sensor.py
+++ src/slow_control/measure/sensor.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import datetime
 import time
 from abc import ABC, abstractmethod
 
@@ -9,7 +10,7 @@ from slow_control.devices.device import Device
 class Sensor(ABC):
     def __init__(self, device: Device):
         self._device: Device = device
-        self._sensor_type: str
+        self._sensor_type: str = ""
         self._measurement_value: any = None
         self._measurement_time: int = 0  # Unix time
 
@@ -24,6 +25,9 @@ class Sensor(ABC):
     def update(self) -> None:
         self._update_measurement()
         self._update_time()
+
+    def __repr__(self):
+        return f"{self.measurement_time_formatted} - {self.measurement_value}"
 
     @abstractmethod
     def _update_measurement(self) -> None:
@@ -40,6 +44,11 @@ class Sensor(ABC):
     @property
     def measurement_time(self):
         return self._measurement_time
+
+    @property
+    def measurement_time_formatted(self):
+        timestamp = datetime.datetime.utcfromtimestamp(self._measurement_time)
+        return timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
     def _update_time(self):
         self._measurement_time = int(time.time())
--- test/test_slow_control.py
+++ test/test_slow_control.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 from slow_control import SlowControl
+from slow_control.devices.vsr53 import VSR53Device
+from slow_control.measure.vsr53 import VSR53Sensor
 
 
 def test_slow_control():
@@ -9,14 +11,33 @@ def test_slow_control():
     # Example usage of the framework
     slow_control = SlowControl()
 
-    state = {"counter": 0}
+    pressure_gauge = VSR53Device("COM5")
+
+    slow_control.add_device("vsr53", pressure_gauge)
+
+    slow_control.add_sensor("pressure_one", VSR53Sensor(pressure_gauge))
+    slow_control.add_sensor("pressure_two", VSR53Sensor(pressure_gauge))
 
     @slow_control.route("/")
     async def root():
-        return state
+        return "slow-control"
+
+    @slow_control.route("/devices")
+    async def devices():
+        return list(slow_control._devices.keys())
+
+    @slow_control.route("/sensors")
+    async def sensors():
+        sensors_with_data = {
+            sensor_name: (sensor.measurement_time_formatted, sensor.measurement_value)
+            for sensor_name, sensor in slow_control._sensors.items()
+        }
+        return sensors_with_data
 
     @slow_control.periodic_task(interval_seconds=1)
     def periodic_task():
-        print("periodic_task")
+        for sensor_name, sensor in slow_control._sensors.items():
+            sensor.update()
+            print(f"{sensor_name}: {sensor}")
 
     slow_control.run()
